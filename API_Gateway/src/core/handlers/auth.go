83b36372486f7db8b88aed396971e6c5b65f98c5
FIZZI-77 <zaprudnovfedor@gmail.com>
2026-05-04 12:17:35 +0300

    add ci

--- API_Gateway/src/core/handlers/auth.go
+++ API_Gateway/src/core/handlers/auth.go
@@ -1,1 +1,11 @@
 package handlers
+
+import authv1 "auth/auth/v1"
+
+type AuthHandler struct {
+	authClient authv1.AuthServiceClient
+}
+
+func NewAuthHandler(authClient authv1.AuthServiceClient) *AuthHandler {
+	return &AuthHandler{authClient: authClient}
+}
